457b1a8441a288f52492237ed793c01aa7adbac4
shiqui <shiqi8sq@gmail.com>
2024-12-24 20:07:51 -0500

    Refactor scanner

---
+++ src/scanner/__init__.py
@@ -0,0 +1,2 @@
+from scanner.token import Token, TokenType, KEYWORD_MAP  # noqa: F401
+from scanner.scanner import Scanner  # noqa: F401
---
+++ src/scanner/token.py
@@ -0,0 +1,88 @@
+from enum import Enum, auto
+from dataclasses import dataclass
+
+
+class TokenType(Enum):
+    # Single-character tokens
+    LEFT_PAREN = auto()
+    RIGHT_PAREN = auto()
+    LEFT_BRACE = auto()
+    RIGHT_BRACE = auto()
+
+    COMMA = auto()
+    DOT = auto()
+    MINUS = auto()
+    PLUS = auto()
+    SEMICOLON = auto()
+    SLASH = auto()
+    STAR = auto()
+
+    # One or two character tokens
+    BANG = auto()
+    BANG_EQUAL = auto()
+
+    EQUAL = auto()
+    EQUAL_EQUAL = auto()
+
+    GREATER = auto()
+    GREATER_EQUAL = auto()
+
+    LESS = auto()
+    LESS_EQUAL = auto()
+
+    # Literals.
+    IDENTIFIER = auto()
+    STRING = auto()
+    NUMBER = auto()
+
+    # Keywords
+    AND = auto()
+    CLASS = auto()
+    ELSE = auto()
+    FALSE = auto()
+    FUN = auto()
+    FOR = auto()
+    IF = auto()
+    NIL = auto()
+    OR = auto()
+
+    PRINT = auto()
+    RETURN = auto()
+    SUPER = auto()
+    THIS = auto()
+    TRUE = auto()
+    VAR = auto()
+    WHILE = auto()
+
+    EOF = auto()
+
+
+KEYWORD_MAP = {
+    "and": TokenType.AND,
+    "class": TokenType.CLASS,
+    "else": TokenType.ELSE,
+    "false": TokenType.FALSE,
+    "for": TokenType.FOR,
+    "fun": TokenType.FUN,
+    "if": TokenType.IF,
+    "nil": TokenType.NIL,
+    "or": TokenType.OR,
+    "print": TokenType.PRINT,
+    "return": TokenType.RETURN,
+    "super": TokenType.SUPER,
+    "this": TokenType.THIS,
+    "true": TokenType.TRUE,
+    "var": TokenType.VAR,
+    "while": TokenType.WHILE,
+}
+
+
+@dataclass
+class Token:
+    type: TokenType
+    lexeme: str
+    literal: object
+    line: int
+
+    def __str__(self):
+        return f"{self.type:20} {self.lexeme:10} {self.literal}"
